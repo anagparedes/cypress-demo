33a54f9379397c45b27c4fc38a8dbca2ff2e81c7
Ana Paredes <anagparedesg@gmail.com>
2022-07-06 18:58:57 -0400

    [ADD] navigation test file

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Navigation', () => {
+it('should navigate to conference sessions page', () => {
+  cy.visit('http: //localhost:1337/conference');
+  cy.get('h1').contains('View Sessions');
+});
+})
